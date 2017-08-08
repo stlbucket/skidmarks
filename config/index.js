fd357c9df9d4e68779d0b7fbafaf1ba0ca6103ba
stlbucket <stlbucket@gmail.com>
2017-08-08 09:32:16 -0500

    rm config

--- config/index.js
+++
@@ -1,8 +0,0 @@
-process.env.DB_CONNECTION_STRING = 'postgres://postgres:fbktp@$sword@localhost/skidmarks'
-process.env.SCHEMAS_TO_GRAPHQL = [ 'skidmarks' ]
-process.env.PG_DEFAULT_ROLE = 'skidmarks_anonymous'
-process.env.GRAPHIQL = true
-process.env.POSTGRAPHQL_SCHEMA = 'skidmarks'
-process.env.JWT_PG_TYPE_IDENTIFIER = 'skidmarks.jwt_token'
-process.env.JWT_SECRET = 'S*PERDuPER$#(R#T'
-process.env.DISABLE_DEFAULT_MUTATIONS = false
