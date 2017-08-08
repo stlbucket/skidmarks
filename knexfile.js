e051875eca01374c9b6fad67cfd68bc8adb48cfd
stlbucket <stlbucket@gmail.com>
2017-08-08 09:25:15 -0500

    knexfile config

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,10 @@
 // Update with your config settings.
+require('./config')
 
 module.exports = {
   skidmarks_dev: {
     client: 'postgresql',
-    connection: 'postgres://postgres:fbktp@$sword@localhost/skidmarks',
+    connection: process.env.DB_CONNECTION_STRING,
     migrations: {
       directory: './src/db/skidmarks/migrations/base',
       tableName: 'knex_migrations'
